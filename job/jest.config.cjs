6c64cefb6e0a9d8dda1d37e5059c34fea001fb2f
Piyawat <arunwatd@gmail.com>
2024-07-25 09:00:12 +0700

    remove not use

--- job/jest.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  displayName: 'Tests Javascript Application - Job',
-  moduleDirectories: ['node_modules', 'src'],
-  testMatch: ['**/?(*.)+(spec|test).[j]s?(x)'],
-  testEnvironment: 'node',
-};
--- job/src/middleware/http.middleware.js
+++
@@ -1,6 +0,0 @@
-/**
- * Configure Middleware. Example only. Adapt on your own
- */
-export const createHttpMiddlewareOptions = (region) => ({
-  host: `https://api.${region}.commercetools.com`,
-});
